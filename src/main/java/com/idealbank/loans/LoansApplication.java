7a3da85207ac7ad095508d1feed3c25ea9c84a3d
vivekkarli <vivekkarli7@gmail.com>
2024-10-13 21:24:00 +0530

    add mqsql dependency

--- src/main/java/com/idealbank/loans/LoansApplication.java
+++ src/main/java/com/idealbank/loans/LoansApplication.java
@@ -4,9 +4,11 @@ import com.idealbank.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
 
 	public static void main(String[] args) {
