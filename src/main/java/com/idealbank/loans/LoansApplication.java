4c4add59396691fbe92e37ee7c72c7467d838243
vivekkarli <vivekkarli7@gmail.com>
2024-10-13 17:48:35 +0530

    first commit

---
+++ src/main/java/com/idealbank/loans/LoansApplication.java
@@ -0,0 +1,16 @@
+package com.idealbank.loans;
+
+import com.idealbank.loans.dto.LoansContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
